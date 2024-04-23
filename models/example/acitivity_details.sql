e4d031fdfda45e3f25b0be5101d7c2aabebea5ec
Eslam Mohamed <eslam.mohamed@nweave.com>
2024-04-23 21:32:07 +0200

    first commit

---
+++ models/example/acitivity_details.sql
@@ -0,0 +1,23 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+SELECT
+    "Activity ID", 
+    CONCAT("Activity Record Type", ' ', "Entity Type") as full_name,
+    entity
+FROM
+    activities
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
